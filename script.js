0c432515b1204f486e7b8486d21159d8e3d418db
redwhizmlfo <yoursmater@gmail.com>
2026-04-27 17:06:38 -0500

    JS: funcion mostrarHora y actualizacion de cambiarMensaje

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function cambiarMensaje() {
     const mensaje = document.getElementById("mensaje");
     mensaje.textContent = "Gracias por hacer clic en el boton.";
+    const extra = document.getElementById("extra");
+    extra.textContent = "Este texto aparecio dinamicamente.";
 }
+
+function mostrarHora() {
+    const ahora = new Date();
+    alert("La hora actual es: " + ahora.toLocaleTimeString());
+}
+
+
